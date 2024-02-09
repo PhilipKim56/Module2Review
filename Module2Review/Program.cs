7e291e0abc0258bc5f5f3d1f358ea15fb43ddbe4
Philip Kim <filshouse56@gmail.com>
2024-02-09 12:12:04 -0500

    Module 2 Review Completed

---
+++ Module2Review/Program.cs
@@ -0,0 +1,50 @@
+﻿using Module2Review;
+
+class Program : Student
+{
+    static void Main(string[] args)
+{
+    List<Student> students = new List<Student>();
+
+    
+    Student student1 = new Student() { Name = "John", ID = 1 };
+    Student student2 = new Student() { Name = "Alice", ID = 2 };
+    Student student3 = new Student();
+    student3.Name = "Bob";
+    student3.ID = 3;
+    Student student4 = new Student();
+    student4.Name = "Eva";
+    student4.ID = 4;
+
+   
+    students.Add(student1);
+    students.Add(student2);
+    students.Add(student3);
+    students.Add(student4);
+
+    
+    student1.AddGrade(94.3);
+    student1.AddGrade(90.0, 78.9, 95.4);
+
+    student2.AddGrade(88.5, 91.2);
+    student2.AddGrade(85.0);
+
+    student3.AddGrade(75.6, 80.3, 77.8);
+
+    student4.AddGrade(92.7);
+
+    
+    foreach (var student in students)
+    {
+        Console.WriteLine($"ID: {student.ID}, Name: {student.Name}");
+        Console.Write("Grades: ");
+        foreach (var grade in student.Grades)
+        {
+            Console.Write($"{grade}, ");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Average Grade: {student.CalculateAverageGrade()}");
+        Console.WriteLine();
+    }
+}
+}
---
+++ Module2Review/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2Review
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public List<double> Grades { get; set; }
+
+        public Student()
+        {
+            Grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+
+        public double CalculateAverageGrade()
+        {
+            if (Grades.Count == 0)
+                return 0;
+
+            double sum = 0;
+            foreach (var grade in Grades)
+            {
+                sum += grade;
+            }
+            return sum / Grades.Count;
+        }
+    }
+}
